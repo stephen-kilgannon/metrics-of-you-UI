81cbf1c50f763d2e6a129d72dde317f8042f1985
Stephen <kilgannon.stephen1@gmail.com>
2022-10-06 23:37:53 +0100

    Added next.js calling api

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import Nav from './Nav'
+import styles from '../styles/Layout.module.css'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Nav />
+      <h3>SUP</h3>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          {children}
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default Layout
---
+++ pages/profile.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const profile = () => {
+  return (
+    <div>
+      <Head>
+        <title>Profile</title>
+      </Head>
+    </div>
+  )
+}
+
+export default profile
