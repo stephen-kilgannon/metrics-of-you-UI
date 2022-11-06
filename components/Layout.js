35ba123281107303cec85c696817d9015532911c
Caolan Gilroy <caolangilroy@gmail.com>
2022-11-06 03:52:27 +0000

    updating style

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,34 @@
-import Nav from "./Nav";
-import styles from "../styles/Layout.module.css";
+import React, {useState} from 'react'
+import SideNav from './SideNav'
+import Header from './Header'
 
-const Layout = ({ children }) => {
+import styles from '../styles/Layout.module.css'
+
+const Layout = ({ props, state, children }) => {
+  const [wid, setWid] = useState('0%');
+//   const openSidenav = ( ) => {
+//     setWid('25%')
+//  }
+// const ref = useRef(null)
+// const [width, setWidth] = useState(0);
+// useLayoutEffect(() => {
+//   setWidth(ref.current.offsetWidth);
+  
+// }, [])
   return (
-    <>
-      <Nav />
+    <div className='container'>
+      <Header></Header>
+
+    <div>
+      {/* <button onClick = {openSidenav}>Open</button> */}
+      <SideNav/>
       <div className={styles.container}>
         <main className={styles.main}>{children}</main>
       </div>
-    </>
-  );
-};
+    {/* </> */}
+    </div>
+    </div>
+  )
+}
 
 export default Layout;
