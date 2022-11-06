694a699338595a2b15880b2d6f0980e769744e6b
Stephen <kilgannon.stephen1@gmail.com>
2022-11-06 00:37:37 +0000

    fix slideshow

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -1,24 +1,40 @@
-import slideshowStyles from '../styles/Slideshow.module.css'
-import Image from 'next/image'
-
-import defaultCardImage from '../public/images/image1.png';
-
-
-const Slideshow = ({imageSrc = defaultCardImage}) => {
-
-    const colors = ["#0088FE", "#00C49F", "#FFBB28"];
-    
-  return (
-    <div>
-      <h1>SLIDE SHOW</h1>
-    <div className={slideshowStyles.slideshow}>
-    <div className={slideshowStyles.slideshowSlider}>
-    <Image src={imageSrc} alt="me" width="64" height="64" />
-    </div>
-  </div>
-
-  </div>
-  )
-}
-
-export default Slideshow
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+  
+export default class NextJsCarousel extends Component {
+    render() {
+        return (
+            <div>
+              <h2>Slideshow</h2>
+              <Carousel>
+                  <div>
+                      <img src="/1.png" alt="image1"/>
+                      <p className="legend">Image 1</p>
+  
+                  </div>
+                  <div>
+                      <img src="/2.png" alt="image2" />
+                      <p className="legend">Image 2</p>
+  
+                  </div>
+                  <div>
+                      <img src="/3.png" alt="image3"/>
+                      <p className="legend">Image 3</p>
+  
+                  </div>
+                  <div>
+                      <img src="/4.png" alt="image4"/>
+                      <p className="legend">Image 4</p>
+  
+                  </div>
+                  <div>
+                      <img src="/5.png" alt="image5"/>
+                      <p className="legend">Image 5</p>
+  
+                  </div>
+              </Carousel>
+            </div>
+        );
+    }
+};
