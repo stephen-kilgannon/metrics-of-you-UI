0f0ce4dbc3bd41b9f34e74d575c6eb41fedcffd3
Stephen <kilgannon.stephen1@gmail.com>
2022-11-10 17:59:40 +0000

    Fixed that for neil

--- components/SideNav.js
+++ components/SideNav.js
@@ -6,7 +6,7 @@ const SideNav = (props) => {
     <nav className={navStyles.nav} style={{width: props.width}}>
        <b><Link href='/'>Home</Link><br></br></b>
 			 <b><Link href='/tide'>Tide</Link><br></br></b>
-			 <b><Link href='/gallary'>Gallary</Link></b>
+			 <b><Link href='/gallery'>Gallery</Link></b>
        <b><Link href='/about'>About</Link></b>
     </nav>
   )
