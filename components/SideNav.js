c5bc0b3d88105c361c7c3b9c0a84d5f1df64578b
NeilByrne97 <neilbyrne19@gmail.com>
2022-11-10 18:23:05 +0000

    Added .ebextensions file

--- components/SideNav.js
+++ components/SideNav.js
@@ -1,13 +1,13 @@
-import Link from 'next/Link'
+import Link from 'next/link'
 import navStyles from '../styles/SideNav.module.css'
 
 const SideNav = (props) => {
   return (
-    <nav className={navStyles.nav} style={{width: props.width}}>
-       <b><Link href='/'>Home</Link><br></br></b>
-			 <b><Link href='/tide'>Tide</Link><br></br></b>
-			 <b><Link href='/gallery'>Gallery</Link></b>
-       <b><Link href='/about'>About</Link></b>
+    <nav className={navStyles.nav} style={{ width: props.width }}>
+      <b><Link href='/'>Home</Link><br></br></b>
+      <b><Link href='/tide'>Tide</Link><br></br></b>
+      <b><Link href='/gallery'>Gallery</Link></b>
+      <b><Link href='/about'>About</Link></b>
     </nav>
   )
 }
